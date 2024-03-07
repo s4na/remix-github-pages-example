332d6be8767520ca7370018b09a7017ff2820811
s4na <appletea.umauma@gmail.com>
2024-03-07 23:59:37 +0900

    s

---
+++ remix-app/vitest.config.ts
@@ -0,0 +1,18 @@
+import * as path from "path";
+import * as VitestConfig from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default VitestConfig.defineConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    includeSource: ["app/**/*.{ts,tsx}"],
+    exclude: ["node_modules", "e2e"],
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "app"),
+    },
+  },
+  plugins: [react()],
+});
