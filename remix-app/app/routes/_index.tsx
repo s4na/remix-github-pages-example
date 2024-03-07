d42695988b2e8320f37f78044ce8164697ba5a94
s4na <appletea.umauma@gmail.com>
2024-03-07 23:47:34 +0900

    記事一覧を表示する

--- remix-app/app/routes/_index.tsx
+++ remix-app/app/routes/_index.tsx
@@ -2,39 +2,33 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Remix App | Articles" },
+    { description: "Articles" },
   ];
 };
+
+const articles = [
+  { title: "article1", url: "https://example/articles/1" },
+  { title: "article2", url: "https://example/articles/2" },
+  { title: "article3", url: "https://example/articles/3" },
+];
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Hello World!</h1>
+      <h1>Articles</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {articles.map((article) => (
+          <li key={article.url}>
+            <a
+              target="_blank"
+              href={article.url}
+              rel="noopener noreferrer"
+            >
+              {article.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
